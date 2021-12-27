1f7e9d24a87bff78e691ebf34f2afe2a2dad9a3f
Balazs Takacs <takibali56@gmail.com>
2021-12-27 18:59:10 +0100

    Refactoring

--- sudoku_solver/sudoku_solver.py
+++ sudoku_solver/sudoku_solver.py
@@ -1,43 +1,48 @@
 import time
 
-start = time.time()
-
-table = [
-    [0, 0, 0, 8, 0, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 4, 3],
-    [5, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 7, 0, 8, 0, 0],
-    [0, 0, 0, 0, 0, 0, 1, 0, 0],
-    [0, 2, 0, 0, 3, 0, 0, 0, 0],
-    [6, 0, 0, 0, 0, 0, 0, 7, 5],
-    [0, 0, 3, 4, 0, 0, 0, 0, 0],
-    [0, 0, 0, 2, 0, 0, 6, 0, 0]
+tables = [
+    [
+        [0, 0, 0, 8, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 4, 3],
+        [5, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 7, 0, 8, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 2, 0, 0, 3, 0, 0, 0, 0],
+        [6, 0, 0, 0, 0, 0, 0, 7, 5],
+        [0, 0, 3, 4, 0, 0, 0, 0, 0],
+        [0, 0, 0, 2, 0, 0, 6, 0, 0]
+    ],
+    [
+        [0, 3, 0, 0, 1, 0, 0, 6, 0],
+        [7, 5, 0, 0, 3, 0, 0, 4, 8],
+        [0, 0, 6, 9, 8, 4, 3, 0, 0],
+        [0, 0, 3, 0, 0, 0, 8, 0, 0],
+        [9, 1, 2, 0, 0, 0, 6, 7, 4],
+        [0, 0, 4, 0, 0, 0, 5, 0, 0],
+        [0, 0, 1, 6, 7, 5, 2, 0, 0],
+        [6, 8, 0, 0, 9, 0, 0, 1, 5],
+        [0, 9, 0, 0, 4, 0, 0, 3, 0]
+    ]
 ]
 
-# table = [
-#     [0, 3, 0, 0, 1, 0, 0, 6, 0],
-#     [7, 5, 0, 0, 3, 0, 0, 4, 8],
-#     [0, 0, 6, 9, 8, 4, 3, 0, 0],
-#     [0, 0, 3, 0, 0, 0, 8, 0, 0],
-#     [9, 1, 2, 0, 0, 0, 6, 7, 4],
-#     [0, 0, 4, 0, 0, 0, 5, 0, 0],
-#     [0, 0, 1, 6, 7, 5, 2, 0, 0],
-#     [6, 8, 0, 0, 9, 0, 0, 1, 5],
-#     [0, 9, 0, 0, 4, 0, 0, 3, 0]
-# ]
+def print_table(table):
+    print("-" * 38)
+    for i in range(3):
+        for j in range(3):
+            print(" | {}  {}  {}  |  {}  {}  {}  |  {}  {}  {}  | ".format(*table[i*3+j]))
+        print("-" * 38)
 
-def solve(x, y):
+
+def solve(x, y, table):
     if table[x][y] == 0:
-        valid_numbers = find_valid_numbers(x, y)
-        if len(valid_numbers) == 0:
+        valid_numbers = find_valid_numbers(x, y, table)
+        if not valid_numbers:
             table[x][y] = 0
             return
         for number in valid_numbers:
             table[x][y] = number
             if x == y == 8:
-                for row in table:
-                    print(row)
-                print(time.time() - start)
+                print_table(table)
                 exit()
             else:
                 i = x
@@ -49,42 +54,28 @@ def solve(x, y):
                     else:
                         i = i + 1
 
-                solve(i, j)
+                solve(i, j, table)
 
             table[x][y] = 0
 
-def find_valid_numbers(x, y):
-    valid_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    for i in range(9):
-        if table[i][y] != 0:
-            try:
-                valid_numbers.remove(table[i][y])
-            except ValueError:
-                pass
+def find_valid_numbers(x, y, table):
+    valid_numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 
     for i in range(9):
-        if table[x][i] != 0:
-            try:
-                valid_numbers.remove(table[x][i])
-            except ValueError:
-                pass
+        valid_numbers.discard(table[i][y])
+        valid_numbers.discard(table[x][i])
     
     x_block = x // 3 * 3
     y_block = y // 3 * 3
     
     for i in range(x_block, x_block + 3):
         for j in range(y_block, y_block + 3):
-            if table[i][j] != 0:
-                try:
-                    valid_numbers.remove(table[i][j])
-                except ValueError:
-                    pass
+            valid_numbers.discard(table[i][j])
 
     return valid_numbers
 
 def main():
-    solve(0, 0)
+    solve(0, 0, tables[0])
 
 if __name__ == "__main__":
     main()
-    print(time.time() - start)
